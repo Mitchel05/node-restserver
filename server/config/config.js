710e5b86c9f674873e80ae95574607511af327fd
Mitchel <megitorep@gmail.com>
2021-01-26 02:48:18 -0500

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,22 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
+
+// ============================
+//  Vencimiento
+// ============================
+//60 segundos
+//60 minutos
+//24 horas
+//30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ============================
+//  Semilla de autenticación
+// ============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
 // ============================
 //  Base de datos
 // ============================
@@ -18,13 +34,14 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    // urlDB = process.env.MONGO_URI;
-    urlDB = 'mongodb+srv://mitchel:hfNyHctoYgGHO1gs@cluster0.vlmus.mongodb.net/cafe';
+     urlDB = process.env.MONGO_URI;
+    
 }
 
 
 
 process.env.URLDB = urlDB;
+
 
 
 
